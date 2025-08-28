61c25aefebe5d1dfdb426133fba9abd8b3d24473
Goodwin <dgoodwin3@una.edu>
2025-08-28 13:33:45 -0500

    testing again 3

--- app.mjs
+++ app.mjs
@@ -1,18 +1,20 @@
 import express from 'express'
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 // import { dirname } from 'node:path';
 // import { fileURLToPath } from 'node:url';
 
 const app = express()
 const PORT = process.env.PORT || 3000; 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')))
-
 // app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
-
-
+app.use(express.static(join(__dirname, 'public')));
 // app.use(express.static(__dirname + 'public'));
 
 
